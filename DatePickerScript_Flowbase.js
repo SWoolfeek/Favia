b1c7be9b7c8f1aa2d10f5f05b0c02c67489fbf73
SWoolfeek <50416553+SWoolfeek@users.noreply.github.com>
2023-12-21 01:21:15 +0100

    Update DatePickerScript_Flowbase.js

--- DatePickerScript_Flowbase.js
+++ DatePickerScript_Flowbase.js
@@ -2,7 +2,7 @@ var mindate = new Date();
 mindate.setDate(mindate.getDate() - 8);
 var maxdate = new Date();
 maxdate.setDate(maxdate.getDate() - 1);
-$('.date-picker').datepicker({
+$('#minMaxExample').datepicker({
     language: 'en',
     range : true,
     minDate : mindate,
